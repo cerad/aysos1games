9d1165e099ec6a69cb7ad2b898be981fafb43503
Rick Roberts <rroberts9@socal.rr.com>
2013-09-28 22:42:53 -0700

    Updates <script></script> html; updated path to css, images, jss

--- web/app.php
+++ web/app.php
@@ -1,6 +1,6 @@
 <?php
 
-define('CERAD_TOURN_SHOW_CONFIG','default');
+define('CERAD_TOURN_SHOW_CONFIG','s1');
 
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
