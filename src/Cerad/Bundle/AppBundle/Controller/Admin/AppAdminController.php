ad1bb4c56c66897447b3f8bda31d04372895b6d1
Rick Roberts <rroberts9@socal.rr.com>
2013-11-14 21:43:40 -0800

    Added function hasRoleScoreAdmin to AppAdminController; exposed Officials export to ROLE_ASSIGNOR

--- src/Cerad/Bundle/AppBundle/Controller/Admin/AppAdminController.php
+++ src/Cerad/Bundle/AppBundle/Controller/Admin/AppAdminController.php
@@ -3,16 +3,23 @@ namespace Cerad\Bundle\AppBundle\Controller\Admin;
 
 use Symfony\Component\HttpFoundation\Request;
 
-use Cerad\Bundle\TournBundle\Controller\BaseController as MyBaseController;
+use Cerad\Bundle\TournBundle\Controller\BaseController as AppBaseController;
 
-class AppAdminController extends MyBaseController
+class AppAdminController extends AppBaseController
 {
-    public function showAction(Request $request)
-    {
-        if ( !$this->hasRoleAdmin() && !$this->hasRoleAssignor() /*&& !$this->hasRoleScoreAdmin()*/ ) return $this->redirect('cerad_tourn_home');
 
-        $tplData = array();
-        $tplName = $request->get('_template');
-        return $this->render($tplName, $tplData);
-    }
+  protected function hasRoleScoreAdmin($projectId = null)
+  {
+    return $this->get('security.context')->isGranted('ROLE_SCORE_ADMIN');
+  }
+
+  public function showAction(Request $request)
+  {
+    if ( !$this->hasRoleAdmin() && !$this->hasRoleAssignor() && !$this->hasRoleScoreAdmin() ) return $this->redirect('cerad_tourn_home');
+
+    $tplData = array();
+    $tplName = $request->get('_template');
+    return $this->render($tplName, $tplData);
+  }
+
 }
